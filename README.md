e27312905a49464fb3baedb7a4b09308c5b0da32
Borislav Hadzhiev <opmftw@gmail.com>
2021-05-04 09:06:17 +0300

    added infrastructure code

---
+++ README.md
@@ -0,0 +1,29 @@
+# VPC Example in AWS CDK - Complete Guide
+
+A repository for an article on
+[bobbyhadz.com](https://bobbyhadz.com/blog/aws-cdk-vpc-example)
+
+## How to Use
+
+1. Clone the repository
+
+2. Install the dependencies
+
+```bash
+npm install
+```
+
+3. Create the CDK stack
+
+```bash
+npx cdk deploy
+```
+
+4. Open the AWS CloudFormation Console and the stack should be created in your
+   default region
+
+5. Cleanup
+
+```bash
+npx cdk destroy
+```
---
+++ lib/cdk-starter-stack.ts
@@ -0,0 +1,75 @@
+/* eslint-disable no-restricted-syntax */
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as cdk from '@aws-cdk/core';
+
+export class CdkStarterStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const vpc = new ec2.Vpc(this, 'my-cdk-vpc', {
+      cidr: '10.0.0.0/16',
+      natGateways: 0,
+      maxAzs: 3,
+      subnetConfiguration: [
+        // {
+        //   name: 'private-subnet-1',
+        //   subnetType: ec2.SubnetType.PRIVATE,
+        //   cidrMask: 24,
+        // },
+        {
+          name: 'public-subnet-1',
+          subnetType: ec2.SubnetType.PUBLIC,
+          cidrMask: 24,
+        },
+        {
+          name: 'isolated-subnet-1',
+          subnetType: ec2.SubnetType.ISOLATED,
+          cidrMask: 28,
+        },
+      ],
+    });
+
+    cdk.Aspects.of(vpc).add(new cdk.Tag('Name', 'my-cdk-vpc'));
+
+    // 👇 replace Name tag for public subnets
+    for (const subnet of vpc.publicSubnets) {
+      cdk.Aspects.of(subnet).add(
+        new cdk.Tag(
+          'Name',
+          `${vpc.node.id}-${subnet.node.id.replace(/Subnet[0-9]$/, '')}-${
+            subnet.availabilityZone
+          }`,
+        ),
+      );
+    }
+
+    // 👇 replace Name tag for private subnets
+    for (const subnet of vpc.privateSubnets) {
+      cdk.Aspects.of(subnet).add(
+        new cdk.Tag(
+          'Name',
+          `${vpc.node.id}-${subnet.node.id.replace(/Subnet[0-9]$/, '')}-${
+            subnet.availabilityZone
+          }`,
+        ),
+      );
+    }
+
+    // 👇 replace Name tag for private subnets
+    for (const subnet of vpc.isolatedSubnets) {
+      cdk.Aspects.of(subnet).add(
+        new cdk.Tag(
+          'Name',
+          `${vpc.node.id}-${subnet.node.id.replace(/Subnet[0-9]$/, '')}-${
+            subnet.availabilityZone
+          }`,
+        ),
+      );
+    }
+
+    new cdk.CfnOutput(this, 'vpcId', {
+      value: vpc.vpcId,
+      description: 'the ID of the VPC',
+    });
+  }
+}
